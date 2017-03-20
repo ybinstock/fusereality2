6f693fb4405402fc83f67650e6283d22f499f63f
ybinstock <ybinstock@gmail.com>
2017-03-19 22:56:48 -0400

    three

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 AFRAME.registerComponent('cursor-listener', {
   init: function() {
-    this.el.addEventListener('click', function(evt) {
+    function functionName(evt) {
       document.querySelector('.light').emit('fade');
       document.querySelector('.light').emit('fade2');
       document.querySelector(".black-sky").emit('fade');
       document.querySelector(".white-sky").emit('fade');
       document.querySelector('.collidable').removeEventListener('cursor-listener', evt);
+      console.log('hi');
+      document.querySelector('.collidable').removeEventListener('click', this);
+    }
 
-    });
-
+    document.querySelector('.collidable').addEventListener('click', functionName);
   }
 });
